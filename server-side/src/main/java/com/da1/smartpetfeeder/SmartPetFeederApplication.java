749ed556a987add627bda679bea7e537dabbcb3b
DA1YAYUAN <836919594@qq.com>
2022-11-14 18:50:44 +0800

    服务端代码

---
+++ server-side/src/main/java/com/da1/smartpetfeeder/SmartPetFeederApplication.java
@@ -0,0 +1,13 @@
+package com.da1.smartpetfeeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPetFeederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPetFeederApplication.class, args);
+	}
+
+}
---
+++ server-side/src/test/java/com/da1/smartpetfeeder/SmartPetFeederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.da1.smartpetfeeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPetFeederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
